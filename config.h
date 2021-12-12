fa9c6cfb0c721c710e745809a3fe0d43cdc6d00d
C. Burton <cjamesburton@outlook.com>
2021-12-12 03:44:38 +0000

    ass

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true"; */
-static char *font = "JetBrainsMono:medium:size=14:antialias=true:hinting=true";
+static char *font = "mono:medium:size=14:antialias=true:hinting=true";
 static int borderpx = 2;
 
 /*
